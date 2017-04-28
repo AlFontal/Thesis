6ca72af4f1b37f18db022d52fd9a9c8a807f8dc9
AlFontal <jandrofontal@gmail.com>
2017-04-28 19:07:31 +0200

    Added first lines of code for toy NN

---
+++ Simple NN.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import numpy as np
+import tensorflow as tf
+import preprocess
+
+sess = tf.InteractiveSession()
+
+seq1 = preprocess.seq1
+seq2 = preprocess.seq2
+test = [sum(preprocess.seq2onehot(seq2), []),
+        sum(preprocess.seq2onehot(seq1), [])]
+
+
+x = tf.placeholder(tf.float32, [None, 60*22])
+y_ = tf.placeholder(tf.string, [None, 1])
+b = tf.Variable(tf.zeros([2]))
+W = tf.Variable(tf.zeros([60*22, 2]))
+
+y = tf.matmul(x, W) + b
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+
+print cross_entropy.eval(feed_dict={x: test, y_: [["A"], ["B"]]})
+
+
+# Work in progress
--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,6 @@
 
 from __future__ import division
 import numpy as np
-import matplotlib.pyplot as plt
 
 __author__ = 'Alejandro Fontal'
 
@@ -11,7 +10,8 @@ aminoacids = "NILVFMCAGOTSYWQBHEDKRP"
 aa_dict = {}
 for idx, aa in enumerate(aminoacids):
     if idx > 0:
-        aa_dict[aa] = np.zeros(idx-1).tolist() + [1] +  np.zeros(22-idx).tolist()
+        aa_dict[aa] = np.zeros(idx-1).tolist() + [1] +\
+                      np.zeros(22-idx).tolist()
     else:
         aa_dict[aa] = [1] + np.zeros(21).tolist()
 
@@ -24,17 +24,12 @@ def seq2onehot(seq):
     return onehot
 
 
-if __name__ == "__main__":
 
-
-    seq1 = "MTSSDTQNNKTLAAMKNFAEQYAKRTDTYFCSDLSVTAVVIEGLARHKEELGSPLCPCRH"
-    seq2 = "YEDKEAEVKNTFWNCPCVPMRERKECHCMLFLTPDNDFAGDAQDIPMETLEEVKASMACP"
-
+seq1 = "MTSSDTQNNKTLAAMKNFAEQYAKRTDTYFCSDLSVTAVVIEGLARHKEELGSPLCPCRH"
+seq2 = "YEDKEAEVKNTFWNCPCVPMRERKECHCMLFLTPDNDFAGDAQDIPMETLEEVKASMACP"
 
 onehot = seq2onehot(seq1)
 
 for aa in onehot:
     print aa
-
-print type(onehot)
 
