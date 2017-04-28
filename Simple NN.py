ccc8bc9fcd7dd469c062cf6ac75b3726e1663d93
AlFontal <jandrofontal@gmail.com>
2017-04-28 19:12:13 +0200

    Corrected some mistakes

--- Simple NN.py
+++ Simple NN.py
@@ -14,17 +14,17 @@ test = [sum(preprocess.seq2onehot(seq2), []),
 
 
 x = tf.placeholder(tf.float32, [None, 60*22])
-y_ = tf.placeholder(tf.string, [None, 1])
+y_ = tf.placeholder(tf.float32, [None, 2])
 b = tf.Variable(tf.zeros([2]))
 W = tf.Variable(tf.zeros([60*22, 2]))
-
 y = tf.matmul(x, W) + b
 
+tf.global_variables_initializer().run()
 cross_entropy = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
 
-print cross_entropy.eval(feed_dict={x: test, y_: [["A"], ["B"]]})
+print cross_entropy.eval(feed_dict={x: test, y_: [[1 , 0], [0 , 1]]})
 
 
 # Work in progress
